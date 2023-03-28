e06249bd66584fc58e5ec4fdb50646661bd929c1
jcarriong <finales.15@gmail.com>
2023-03-28 08:22:55 -0500

    first-commit

---
+++ src/main/java/com/bootcamp/bankingproduct/BankingProductApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bankingproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/bankingproduct/BankingProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bankingproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
